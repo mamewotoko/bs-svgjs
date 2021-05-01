7eb4cec32224dea10ef6c2a1e4daede3483b5cb6
Takashi Masuyama <mamewotoko@gmail.com>
2021-05-01 15:10:25 +0900

    create js dir

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 # build sample
 set -e
 npm run build
-#cp node_modules/svgjs/dist/svg.js js/
-#$(npm bin)/browserify -t babelify src/svgjs.bs.js src/main.bs.js -o js/main.js
-#$(npm bin)/browserify src/main.bs.js -o js/main.js
-#$(npm bin)/browserify src/svg.bs.js -o js/svgapp.js
-
+mkdir js
 $(npm bin)/browserify -t babelify src/main.bs.js > js/main.browser.js
+
+# TODO; remove 
 # by loading Svgjs, SVG is defined as a function
 sed -i -e 's/Svgjs.SVG/SVG/g' js/main.browser.js
