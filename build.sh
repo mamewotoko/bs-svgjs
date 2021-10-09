706702e11ae7dc88139fa82c073be842a85a94a4
Takashi Masuyama <mamewotoko@gmail.com>
2021-10-09 17:19:01 +0900

    remove bsdoc from packege.json

--- build.sh
+++ build.sh
@@ -10,15 +10,6 @@ $(npm bin)/browserify -t babelify examples/src/main.bs.js > examples/js/main.bro
 
 cp node_modules/@svgdotjs/svg.js/dist/svg.min.js examples/js
 
-# skip building doc
-exit 0
-
 # bsdoc for linux/windows are not included in bsdoc package
-UNAME=$(uname)
-if [ "$UNAME" = "Darwin" ]; then
-    $(npm bin)/yarn add bsdoc@6.0.4-alpha
-elif [ "$UNAME" = "Linux" ]; then
-    npm install @mauna-ai/bsdoc-linux
-fi
 npm run doc-init
 npm run build:doc
--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
+set -e
 UNAME=$(uname)
 if [ "$UNAME" = "Darwin" ]; then
     brew install npm
+    npm update npm
+    npm install
+    $(npm bin)/yarn add bsdoc@6.0.4-alpha
 elif [ "$UNAME" = "Linux" ]; then
     sudo apt update
     sudo apt install -y npm
+    npm update npm
+    npm install @mauna-ai/bsdoc-linux
+    npm install
 fi
-npm update npm
-npm install
