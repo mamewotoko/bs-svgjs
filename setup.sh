aafedc14ff39a7164e15b71e6d5debaa4d503795
Takashi Masuyama <mamewotoko@gmail.com>
2021-10-09 18:21:14 +0900

    modify yarn path

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ "$UNAME" = "Darwin" ]; then
     brew install npm yarn
     npm update npm
     npm install
-    $(npm bin)/yarn add bsdoc@6.0.4-alpha
+    yarn add bsdoc@6.0.4-alpha
 elif [ "$UNAME" = "Linux" ]; then
     sudo apt update
     sudo apt install -y npm
